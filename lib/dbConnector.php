20a01dfcb2831ae5fe49e9093c61c8992413d9ac
naphat44128 <naphat44128@172.22.26.219>
2016-07-20 11:15:36 +0700

    Add file project

--- lib/dbConnector.php
+++ lib/dbConnector.php
@@ -6,7 +6,6 @@ $dbName = "inventory_management";
 //----------------------------
 $host = "localhost";
 
-
 $dbLink = mysql_connect($host, $user, $pass) or die("Can not connect to database");
 mysql_select_db("$dbName", $dbLink) or die ("Can not select DB");
 
