4e25ba1dac20e52169992dadc9b74b0cffe140f4
naphat44128 <naphat44128@172.22.26.219>
2016-07-20 11:36:20 +0700

    Add file project

---
+++ model/customer.php
@@ -0,0 +1,54 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+class Employee {
+
+    public $sql;
+
+    public function insert($data) {
+        $this->sql = "INSERT INTO tb_customer (`id`, `fullname`, `mobile`, `email`, `address`, `line`) VALUES (NULL, '{$data["name"]}', '{$data["mobile"]}', '{$data["email"]}', '{$data["address"]}', '{$data["line"]}') ";
+        mysql_query("SET NAMES 'utf8'");
+		$query = mysql_query($this->sql);
+        if ($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function delete($condition) {
+        $this->sql = "DELETE FROM tb_customer WHERE {$condition}";
+        mysql_query("SET NAMES 'utf8'");
+		$query = mysql_query($this->sql);
+        if ($query) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+     public function update($data, $user_ref) {
+        $this->sql = "UPDATE tb_customer SET name = '{$data["name"]}', mobile = '{$data["mobile"]}', email = '{$data["email"]}', address='{$data["address"]}', title = '{$data["line"]}'  WHERE user_ref = {$user_ref}";
+        mysql_query("SET NAMES 'utf8'");
+		$query = mysql_query($this->sql);
+        
+        if ($query){		
+                return true;
+        } else {
+            return false;
+        }
+    }
+    
+   public function read() {
+        $this->sql = " SELECT * FROM `tb_customer` ORDER BY id ";
+		mysql_query("SET NAMES 'utf8'");
+        $query = mysql_query($this->sql);
+        if ($query) {
+            $result = array();
+            while ($row = mysql_fetch_array($query, MYSQL_ASSOC)) {
+                $result[] = $row;
+            }
+            return $result;
+        } else {
+            return false;
+        }
+    }
+}
