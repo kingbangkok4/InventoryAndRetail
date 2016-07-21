ede8f68f2fffc35563c8d68e61c7005e1724ef80
naphat44128 <naphat44128@172.22.26.154>
2016-07-21 13:51:20 +0700

    Add file project

--- views/customerList.php
+++ views/customerList.php
@@ -15,20 +15,7 @@ $rows = $obj->read();
                 $('#tableData').paging({limit:10});
             });
         </script>
-        <script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+      
   <style type="text/css">
 
 .paging-nav {
